5d4d1345234360399eeff0a256747b2ecb57a79f
oleksmarkh <olexiy.markhovsky@gmail.com>
2018-01-18 08:28:26 +0100

    chore: introduce utils

--- client/src/config.ts
+++ client/src/config.ts
@@ -6,12 +6,15 @@ const config = {
       center: ([50.45, 30.52] as L.LatLngExpression), // Kyiv
       zoom: 4,
     },
-    tileLayerOptions: {
-      // tslint:disable-next-line max-line-length
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
-      minZoom: 2,
-      maxZoom: 6,
-      id: 'mapbox.light',
+    tileLayer: {
+      urlTemplate: 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
+      options: {
+        // tslint:disable-next-line max-line-length
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
+        minZoom: 2,
+        maxZoom: 6,
+        id: 'mapbox.light',
+      },
     },
   },
 };
